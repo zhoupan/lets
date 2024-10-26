12560779cce4fdfe8e3577d47a96ba7b411f1862
zhoupan <fitzhoupan@dingtalk.com>
2024-10-26 12:59:30 +0800

    Create Math.java

---
+++ java-lib-example/src/main/java/org/example/Math.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 zhoupan (https://github.com/zhoupan).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.example;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+
+public class Math {
+  public static int version() {
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello Shared Library!!");
+    System.exit(0);
+  }
+
+  @CEntryPoint(name = "sum")
+  public static double sum(IsolateThread thread, double a, double b) {
+    return a + b;
+  }
+}
