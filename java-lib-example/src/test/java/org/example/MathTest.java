56ef0b81706e4ce3a7f0a11b4aab7c7733d39ecb
zhoupan <fitzhoupan@dingtalk.com>
2024-10-26 12:59:33 +0800

    Create MathTest.java

---
+++ java-lib-example/src/test/java/org/example/MathTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 zhoupan (https://github.com/zhoupan).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MathTest {
+  @Test
+  public void someLibraryMethodReturnsTrue() {
+    assertEquals(1, Math.version());
+    // assertTrue(Math.sum(0, 0) >= 0);
+  }
+}
