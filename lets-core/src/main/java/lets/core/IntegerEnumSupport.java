5ed050fb1a04400ecfda2e24ffc02ddb51e205a0
zhoupan <fitzhoupan@dingtalk.com>
2024-10-29 18:23:40 +0800

    Create IntegerEnumSupport.java

---
+++ lets-core/src/main/java/lets/core/IntegerEnumSupport.java
@@ -0,0 +1,7 @@
+package lets.core;
+
+public class IntegerEnumSupport extends EnumSupport<Integer>{
+  public IntegerEnumSupport(Integer integer, String message, String description) {
+    super(integer, message, description);
+  }
+}
