c3db66cd7d1aeecb9a37789ba7e771fee1c7a88f
zhoupan <fitzhoupan@dingtalk.com>
2024-11-19 15:50:11 +0800

    format

--- lets-vuejs/eslint.config.js
+++ lets-vuejs/eslint.config.js
@@ -17,18 +17,15 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
-    files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
-    ],
+    files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
   },
   skipFormatting,
 ]
